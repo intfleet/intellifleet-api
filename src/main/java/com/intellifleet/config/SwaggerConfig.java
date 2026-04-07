efdf8b246b246206046b3c1b72e4a061ecca668f
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-04-07 11:17:50 +0530

    code changes

--- src/main/java/com/intellifleet/config/SwaggerConfig.java
+++ src/main/java/com/intellifleet/config/SwaggerConfig.java
@@ -7,10 +7,10 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class SwaggerConfig {
 
-    @Bean
+//    @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
