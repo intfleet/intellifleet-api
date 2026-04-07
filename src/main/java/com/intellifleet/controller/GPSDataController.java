a587430ec86915ec60a3b4b4b31401013c8cabde
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-04-07 11:01:11 +0530

    code changes

--- src/main/java/com/intellifleet/controller/GPSDataController.java
+++ src/main/java/com/intellifleet/controller/GPSDataController.java
@@ -9,8 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/gpsdata")
 @RestController
 @Slf4j
 public class GPSDataController implements GpsDataAPI {
