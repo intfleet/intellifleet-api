feb37ea5d82d1049b796be6f8f64f6122e1f951d
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-04-12 12:41:01 +0530

    code changes

--- src/main/java/com/intellifleet/IntellifleetApiApplication.java
+++ src/main/java/com/intellifleet/IntellifleetApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntellifleetServiceApplication {
+public class IntellifleetApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntellifleetServiceApplication.class, args);
+		SpringApplication.run(IntellifleetApiApplication.class, args);
 	}
 
 }
